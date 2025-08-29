8496a860bc3ef5ce1eb221934abd639638c5244e
Gael2323 <lg.herrera@mail.austral.edu.ar~>
2025-08-29 12:59:43 -0300

    lito

--- slice_simple.py
+++ slice_simple.py
@@ -1,9 +1,9 @@
-#def slice_simple():
-texto = "Awesome"
-texto2 = texto.lower()
-print(texto2[0:3])
-print(texto2[2:6])
-print(f"{texto2[0:4]}{texto2[-3:]}")
+def slice_simple():
+    texto = "Awesome"
+    texto2 = texto.lower()
+    print(texto2[0:3])
+    print(texto2[2:6])
+    print(f"{texto2[0:4]}{texto2[-3:]}")
     # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
     # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
 
