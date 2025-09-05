1a8de8f0330a5adbda8c816b0d4ae3aaa7099883
Gael2323 <lgherrera@mail.austral.edu.ar>
2025-09-05 09:23:25 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -2,5 +2,5 @@ def slice_simple():
     texto = "Awesome"
     texto2 = texto.lower()
     print(texto2[0:3])
-    print(texto2[2:6])
+    print(texto2[2:5])
     print(f"{texto2[0:4]}{texto2[-3:]}")
