7af17e0d5d03353cd719a138c2580e095e84630c
Gael2323 <lg.herrera@mail.austral.edu.ar~>
2025-09-05 09:12:40 -0300

    enter

--- in_string.py
+++ in_string.py
@@ -1,8 +1,8 @@
 def check_vowels():
     x = input()
     palabra = x.lower()
-    print(f"Contiene a:","a" in palabra)
-    print(f"Contiene e:","e" in palabra)
-    print(f"Contiene i:","i"in palabra})
-    print(f"Contiene o:","o" in palabra)
-    print("Contiene u:","u" in palabra")
+    print("Contiene a:","a" in palabra)
+    print("Contiene e:","e" in palabra)
+    print("Contiene i:","i"in palabra)
+    print("Contiene o:","o" in palabra)
+    print("Contiene u:","u" in palabra)
--- slice_simple.py
+++ slice_simple.py
@@ -3,3 +3,4 @@ def slice_simple():
     print(texto[:3].lower())
     print(texto[2:5])
     print(texto[0:4].lower + texto [-3:])
+    
