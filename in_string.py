41ca09c9a7fa73dfac71bcb8dd227435bf03dfc2
Gael2323 <lgherrera@mail.austral.edu.ar>
2025-09-05 09:09:48 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -3,11 +3,6 @@ def check_vowels():
     palabra = x.lower()
     print(f"Contiene a:","a" in palabra)
     print(f"Contiene e:","e" in palabra)
-    print(f"Contiene i:", in palabra})
+    print(f"Contiene i:","i"in palabra})
     print(f"Contiene o:","o" in palabra)
-    print("Contiene u:", "u" in palabra")
-    # Código a implementar utilizando input.
-
-
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
+    print("Contiene u:","u" in palabra")
