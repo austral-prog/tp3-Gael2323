5f6ac02d1aa01c216756acbe9785d710acefd794
Gael2323 <lg.herrera@mail.austral.edu.ar~>
2025-09-05 09:13:11 -0300

    enter

--- slice_simple.py
+++ slice_simple.py
@@ -2,5 +2,4 @@ def slice_simple():
     texto = "Awesome"
     print(texto[:3].lower())
     print(texto[2:5])
-    print(texto[0:4].lower + texto [-3:])
-    
+    print(texto[0:4].lower() + texto[-3:])
